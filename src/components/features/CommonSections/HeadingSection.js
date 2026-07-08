f9a1aa7dd6c8461b3d3d51627f5bc7c5f8efd08c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-08 22:18:18 +0600

    refactor: add spacing between title and paragraph

--- src/components/features/CommonSections/HeadingSection.js
+++ src/components/features/CommonSections/HeadingSection.js
@@ -1,7 +1,7 @@
 export default function HeadingSection({ title, paragraph }) {
     return (
         <section className="w-full border-b border-black/5 pt-[126px] pb-[63px] transition-all duration-200 ease-linear md:pt-[158px] md:pb-[79px]">
-            <div className="mx-auto w-full max-w-7xl text-center">
+            <div className="mx-auto w-full max-w-7xl space-y-4 text-center">
                 <h1 className="text-[32px] leading-tight font-semibold text-gray-950 transition-all duration-200 ease-linear md:text-[44px] lg:text-[56px]">
                     {title}
                 </h1>
