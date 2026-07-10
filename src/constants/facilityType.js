0aebb29d31dbcd62c2c2ffde6d615ffffd2ae821
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-10 21:05:16 +0600

    feat: add facility type and implement facility type enum

---
+++ src/constants/facilityType.js
@@ -0,0 +1,26 @@
+export const facilityType = [
+    {
+        id: 1,
+        type: 'Football Turf',
+    },
+    {
+        id: 2,
+        type: 'Cricket Turf',
+    },
+    {
+        id: 3,
+        type: 'Badminton Court',
+    },
+    {
+        id: 4,
+        type: 'Tennis Court',
+    },
+    {
+        id: 5,
+        type: 'Basketball Court',
+    },
+    {
+        id: 6,
+        type: 'Swimming Pool',
+    },
+];
